191b77ce20f13f79ec19c3dbb86ad9db230523d4
Bill McKessy <bill@evident.io>
2017-06-10 07:55:08 -0700

    stats object

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,97 @@
 <template>
   <div id="app">
     <a-scene>
-      <template v-for='(cube,index) in cubes'>
+      <!-- <template v-for='(cube,index) in cubes'>
         <a-box :position="pos(index)" rotation="0 45 0" color="#4CC3D9"></a-box>
-      </template>
+      </template> -->
 
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+      <a-entity position='0 1.5 -3'>
+        <template v-for="(stat,index) in stats">
+          <a-entity :rotation="[0, index * -5, 0].join(' ')" :position="[index*1.1, 0, 0].join(' ')">
+            <stat v-bind:label='stat.label'
+              v-bind:value='stat.value'
+              v-bind:stat_high='2'
+              v-bind:stat_medium='1'
+              v-bind:stat_low='1'
+              v-bind:stat_pass='1'></stat>
+          </a-entity>
+        </template>
+        <!-- <stat label='us-west-2' value='128' stat_high='2' stat_medium='1' stat_low='1' stat_pass='1'></stat> -->
+      </a-entity>
+      <!-- <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
       <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
       <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <a-text position='0 2 -3' color='#000000' :value="foo"></a-text>
+      <a-text position='0 2 -3' color='#000000' :value="foo"></a-text> -->
       <a-sky color="#ECECEC"></a-sky>
+
+      <a-assets>
+        <a-asset-item id="tree-obj" src="/obj/evident-shield.obj"></a-asset-item>
+        <a-asset-item id="tree-mtl" src="/obj/evident-shield.mtl"></a-asset-item>
+      </a-assets>
+      <!-- <a-entity id='shield' position="0 2 -3" obj-model="obj: #tree-obj; mtl: #tree-mtl"></a-entity> -->
+      <!-- <a-entity id='shield' position="0 2 -3" obj-model="obj: #tree-obj;" material="color: #3366ff; roughness: 1; metalness: 0"></a-entity> -->
+      <!-- <a-entity id='shield' position="0 2 -3" obj-model="obj: #tree-obj;" material="shader: custom-material"></a-entity> -->
+
     </a-scene>
   </div>
 </template>
 
 <script>
 var aframe = require('aframe')
+import stat from './components/stat.vue'
 
 export default {
   name: 'app',
-  components: {},
+  components: {
+    stat
+  },
   data () {
     return {
       foo: '1',
-      cubes: []
+      cubes: [],
+      stats: []
     }
+  },
+  beforeCreate () {
   },
   mounted () {
     console.log(aframe)
     var self = this
+
+    // stats
+    this.stats.push({
+      label: 'us-west-2',
+      value: '32',
+      stat_high: '1',
+      stat_medium: '2',
+      stat_low: '3',
+      stat_pass: '1'
+    })
+    this.stats.push({
+      label: 'us-west-1',
+      value: '100',
+      stat_high: '1',
+      stat_medium: '2',
+      stat_low: '3',
+      stat_pass: '1'
+    })
+    this.stats.push({
+      label: 'us-east-2',
+      value: '5',
+      stat_high: '1',
+      stat_medium: '2',
+      stat_low: '3',
+      stat_pass: '1'
+    })
+
+
     this.cubes.push(0)
     this.cubes.push(0)
     this.cubes.push(0)
     setInterval(function(){
       self.foo = Date.now()
     },1000)
+    // k.object3D.children[0].children[0].material.materials[0].color.g
   },
   methods: {
     pos: function(index){
---
+++ src/components/stat.vue
@@ -0,0 +1,57 @@
+<template>
+  <a-entity id='stat'>
+    <a-text font='roboto' align='center' scale='0.5 0.5 0.5' position='0 0 0' color='#000000' :value="label"></a-text>
+    <a-text font='roboto' align='center' scale='2 2 0' position='0 0.25 0' color='#000000' :value="value"></a-text>
+    <!-- <a-entity id='foo' geometry="primitive: box" material="color: red"></a-entity> -->
+    <a-entity position='-0.25 -0.25 0' scale='1 0.2 0.2'>
+      <a-box geometry="primitive: box" :width='widths[0]' :position="[box_offsets[0], 0, 0].join(' ')" material="color: red; wireframe: false"></a-box>
+      <a-box geometry="primitive: box" :width='widths[1]' :position="[box_offsets[1] - widths[1] * 0.5, 0, 0].join(' ')" material="color: orange; wireframe: false"></a-box>
+      <a-box geometry="primitive: box" :width='widths[2]' :position="[box_offsets[2] - widths[2] * 0.5, 0, 0].join(' ')" material="color: yellow; wireframe: false"></a-box>
+      <a-box geometry="primitive: box" :width='widths[3]' :position="[box_offsets[3] - widths[3] * 0.5, 0, 0].join(' ')" material="color: blue; wireframe: false"></a-box>
+    </a-entity>
+  </a-entity>
+</template>
+
+<script>
+export default {
+  name: 'stat',
+  props: [
+    'label',
+    'value',
+    'stat_high',
+    'stat_medium',
+    'stat_low',
+    'stat_pass'
+  ],
+  data () {
+    return {
+      stat_total: 0,
+      box_offsets: [],
+      widths: []
+    }
+  },
+  mounted () {
+    console.log(this.$el.querySelector('#foo'))
+    window.k = this.$el.querySelector('#foo')
+    this.stat_total = Number(this.stat_high) + Number(this.stat_medium) + Number(this.stat_low) + Number(this.stat_pass)
+    var running_x = 0
+    this.box_offsets.push(running_x)
+    this.widths.push(this.stat_high / this.stat_total)
+    running_x += (this.stat_high / this.stat_total)
+    this.box_offsets.push(running_x)
+    this.widths.push(this.stat_medium / this.stat_total)
+    running_x += (this.stat_medium / this.stat_total)
+    this.box_offsets.push(running_x)
+    this.widths.push(this.stat_low / this.stat_total)
+    running_x += (this.stat_low / this.stat_total)
+    this.box_offsets.push(running_x)
+    this.widths.push(this.stat_pass / this.stat_total)
+    console.log(this.stat_total)
+    console.log(this.box_offsets)
+    console.log(this.widths)
+    console.log(this.$el.object3D)
+    window.k = (this.$el.object3D)
+    this.$el.object3D.lookAt(new THREE.Vector3(1.0,100.0,1.0))
+  }
+}
+</script>
